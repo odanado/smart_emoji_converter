de6bf0bef7485205b7c0e62ddd2a62e7fa03fcda
odan <odan3240@gmail.com>
2017-06-22 23:52:14 +0900

    calc_hash -> calc_feature in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -6,18 +6,17 @@ import json
 from PIL import Image
 from tqdm import tqdm
 
-from utils import calc_hash
+from utils import calc_feature
 
 
 def main(args):
-    ret = {'size': int(args.image_size), 'emoji': {}}
-    size = [int(args.image_size) for _ in range(2)]
+    ret = {}
 
     for filename in tqdm(os.listdir(args.imgs_dir)):
         path = os.path.join(args.imgs_dir, filename)
-        k = calc_hash(path, size)
+        k = calc_feature(path)
         name, _ = os.path.splitext(filename)
-        ret['emoji'][k] = name
+        ret[k] = name
 
     with open(args.save_file, 'w') as f:
         json.dump(ret, f)
@@ -28,7 +27,6 @@ if __name__ == '__main__':
         description='preprocess for smart emoji converter')
     parser.add_argument('imgs_dir')
     parser.add_argument('save_file')
-    parser.add_argument('image_size')
 
     args = parser.parse_args()
     main(args)
